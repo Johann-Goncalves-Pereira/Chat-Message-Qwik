5498dd84f9d2d4dba75ab1fc733fbf96514ebd27
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-10-10 21:17:46 -0300

    feat: index

--- src/components/User/index.ts
+++ src/components/User/index.ts
@@ -1,4 +1,5 @@
-import { UserHeader, UserOptions } from "./UserHeader/UserHeader";
+import { UserHeader } from "./UserHeader/UserHeader";
 import { UserPhoto } from "./UserPhoto/UserPhoto";
+import { UserOptions } from "./UserOptions/UserOptions";
 
 export { UserHeader, UserPhoto, UserOptions };
